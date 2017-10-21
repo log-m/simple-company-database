a3ff5afad2163a3bbdfae98f98768bea3584913f
log-m <loganmorris15@yahoo.com>
2017-10-21 01:23:14 -0500

    all the files

---
+++ JDBCProjectForStudent/src/cs4347/jdbcProject/ecomm/dao/impl/ProductDaoImpl.java
@@ -0,0 +1,164 @@
+package cs4347.jdbcProject.ecomm.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import cs4347.jdbcProject.ecomm.dao.ProductDAO;
+import cs4347.jdbcProject.ecomm.entity.Product;
+import cs4347.jdbcProject.ecomm.util.DAOException;
+
+public class ProductDaoImpl implements ProductDAO
+{
+	private static final String insertSQL = 
+			"INSERT INTO PRODUCT (prodName, prodDescription, prodCategory, prodUPC"
+			+ "VALUES(?, ?, ?, ?)";
+	private static final String retrieveSQL = 
+			"SELECT * FROM PRODUCT WHERE ? = ?";
+	private static final String updateSQL =
+			"UPDATE PRODUCT SET prodName = ?, prodDescription = ?, prodCategory = ?, prodUPC = ? WHERE id = ?";
+	private static final String  deleteSQL =
+			"DELETE FROM PRODUCT WHERE id = ?";
+	public Product create(Connection connection, Product product) throws SQLException, DAOException
+	{
+		if(product.getId() != null) {
+			throw new DAOException("Trying to insert Product with NON-NULL ID");
+		}
+		
+		PreparedStatement ps = null;
+		try {
+			ps = connection.prepareStatement(insertSQL, Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, product.getProdName());
+			ps.setString(2, product.getProdDescription());
+			ps.setInt(3, product.getProdCategory());
+			ps.setString(4, product.getProdUPC());
+			
+			int res = ps.executeUpdate();
+			if(res != 1) {
+				throw new DAOException("Did Not Create Expected Number Of Rows");
+			}
+			ResultSet krs = ps.getGeneratedKeys();
+			krs.next();
+			int autoID = krs.getInt(1);
+			product.setId((long) autoID);
+			return product;
+		}
+		finally {
+			if(ps != null && !ps.isClosed()) {
+				ps.close();
+			}
+		}
+	}
+	
+	public Product retrieve(Connection connection, Long id) throws SQLException, DAOException{
+		if(id == null) {
+			throw new DAOException("Trying to retrieve Product with NULL ID");
+		}
+		PreparedStatement ps = null;
+		try {
+			ps = connection.prepareStatement(retrieveSQL);
+			ps.setString(1, "id");
+			ps.setLong(2, id);
+			
+			ResultSet rs = ps.executeQuery();
+			if(!rs.next()) {
+				return null;
+			}
+			rs.last();
+			if(rs.getRow() != 1) {
+					  throw new DAOException("Did Not Retrieve Expected Number Of Rows");
+			}
+			else {
+				Product prod = new Product();
+				prod.setId(rs.getLong("id"));
+				prod.setProdDescription(rs.getString("prodDescription"));
+				prod.setProdCategory(rs.getInt("prodCategory"));
+				prod.setProdUPC(rs.getString("prodUPC"));
+				return prod;
+					  
+			}
+			
+			
+		}
+		finally {
+			if(ps != null && !ps.isClosed()) {
+				ps.close();
+			}
+		}
+	}
+	public List<Product> retrieveByCategory(Connection connection, int cat) throws SQLException, DAOException{
+		
+		PreparedStatement ps = null;
+		try {
+			ps = connection.prepareStatement(retrieveSQL);
+			ps.setString(1, "prodCategory");
+			ps.setInt(2, cat);
+			
+			ResultSet rs = ps.executeQuery();
+			if(!rs.next()) {
+				return null;
+			}
+			rs.beforeFirst();
+			List<Product> prodList = new ArrayList<Product>();
+			while(rs.next()) {
+				Product prod = new Product();
+				prod.setId(rs.getLong("id"));
+				prod.setProdDescription(rs.getString("prodDescription"));
+				prod.setProdCategory(rs.getInt("prodCategory"));
+				prod.setProdUPC(rs.getString("prodUPC"));
+				prodList.add(prod);
+					  
+			}
+			return prodList;
+			
+			
+		}
+		finally {
+			if(ps != null && !ps.isClosed()) {
+				ps.close();
+			}
+		}
+	}
+	
+	public Product retrieveByUPC(Connection connection, String UPC) throws SQLException, DAOException{
+		if(UPC == null) {
+			throw new DAOException("Trying to retrieve Product with NULL UPC code");
+		}
+		PreparedStatement ps = null;
+		try {
+			ps = connection.prepareStatement(retrieveSQL);
+			ps.setString(1, "prodUPC");
+			ps.setString(2, UPC);
+			
+			ResultSet rs = ps.executeQuery();
+			if(!rs.next()) {
+				return null;
+			}
+			rs.last();
+			if(rs.getRow() != 1) {
+					  throw new DAOException("Did Not Retrieve Expected Number Of Rows");
+			}
+			else {
+				Product prod = new Product();
+				prod.setId(rs.getLong("id"));
+				prod.setProdDescription(rs.getString("prodDescription"));
+				prod.setProdCategory(rs.getInt("prodCategory"));
+				prod.setProdUPC(rs.getString("prodUPC"));
+				return prod;
+					  
+			}
+			
+			
+		}
+		finally {
+			if(ps != null && !ps.isClosed()) {
+				ps.close();
+			}
+		}
+	}
+	
+}
---
+++ JDBCProjectForStudent/src/cs4347/jdbcProject/ecomm/services/impl/ProductPersistenceServiceImpl.java
@@ -0,0 +1,164 @@
+package cs4347.jdbcProject.ecomm.services.impl;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import cs4347.jdbcProject.ecomm.services.ProductPersistenceService;
+import cs4347.jdbcProject.ecomm.dao.ProductDAO;
+import cs4347.jdbcProject.ecomm.entity.Product;
+import cs4347.jdbcProject.ecomm.util.DAOException;
+import cs4347.jdbcProject.ecomm.dao.impl.ProductDaoImpl;
+
+public class ProductPersistenceServiceImpl implements ProductPersistenceService
+{
+	private DataSource dataSource;
+
+	public ProductPersistenceServiceImpl(DataSource dataSource)
+	{
+		this.dataSource = dataSource;
+	}
+	
+	public Product create(Product product) throws SQLException, DAOException
+	{
+		ProductDAO prodDAO = new ProductDaoImpl();
+		Connection conn = dataSource.getConnection();
+		try {
+			conn.setAutoCommit(false);
+			Product prod = prodDAO.create(conn, product);
+			Long prodID = prod.getId();
+			conn.commit();
+			return prod;
+			
+		}
+		catch (Exception ex) {
+			conn.rollback();
+			throw ex;
+		}
+		finally {
+			if(conn != null) {
+				conn.setAutoCommit(true);
+			}
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		}
+	}
+	public Product retrieve(Long id) throws SQLException, DAOException{
+		ProductDAO prodDAO = new ProductDaoImpl();
+		Connection conn = dataSource.getConnection();
+		try {
+			conn.setAutoCommit(false);
+			Product prod = prodDAO.retrieve(conn, id); //throw null ID DAOException in here
+			conn.commit();
+			return prod;
+		}
+		catch(Exception ex) {
+			conn.rollback();
+			throw ex;
+		}
+		finally {
+			if(conn != null) {
+				conn.setAutoCommit(true);
+			}
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		}
+	}
+	
+	public int update(Product product) throws SQLException, DAOException{
+		ProductDAO prodDAO = new ProductDaoImpl();
+		Connection conn = dataSource.getConnection();
+		try {
+			conn.setAutoCommit(false);
+			int count = prodDAO.update(conn, product); //throw null ID DAOException in here
+			conn.commit();
+			return count;
+		}
+		catch(Exception ex) {
+			conn.rollback();
+			throw ex;
+		}
+		finally {
+			if(conn != null) {
+				conn.setAutoCommit(true);
+			}
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		}
+	}
+	
+	public int delete(Long id) throws SQLException, DAOException{
+		ProductDAO prodDAO = new ProductDaoImpl();
+		Connection conn = dataSource.getConnection();
+		try {
+			conn.setAutoCommit(false);
+			int count = prodDAO.delete(conn, id); //throw null ID DAOException in here
+			conn.commit();
+			return count;
+		}
+		catch(Exception ex) {
+			conn.rollback();
+			throw ex;
+		}
+		finally {
+			if(conn != null) {
+				conn.setAutoCommit(true);
+			}
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		}
+	}
+	
+	public Product retrieveByUPC(String upc) throws SQLException, DAOException{
+		ProductDAO prodDAO = new ProductDaoImpl();
+		Connection conn = dataSource.getConnection();
+		try {
+			conn.setAutoCommit(false);
+			Product prod = prodDAO.retrieveByUPC(conn, upc); //throw null ID DAOException in here
+			conn.commit();
+			return prod;
+		}
+		catch(Exception ex) {
+			conn.rollback();
+			throw ex;
+		}
+		finally {
+			if(conn != null) {
+				conn.setAutoCommit(true);
+			}
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		}
+	}
+	
+	public List<Product> retrieveByCategory(int category) throws SQLException, DAOException{
+		ProductDAO prodDAO = new ProductDaoImpl();
+		Connection conn = dataSource.getConnection();
+		try {
+			conn.setAutoCommit(false);
+			List<Product> prods = prodDAO.retrieveByCategory(conn, category); //throw null ID DAOException in here
+			conn.commit();
+			return prods;
+		}
+		catch(Exception ex) {
+			conn.rollback();
+			throw ex;
+		}
+		finally {
+			if(conn != null) {
+				conn.setAutoCommit(true);
+			}
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		}
+	}
+
+}
+	
